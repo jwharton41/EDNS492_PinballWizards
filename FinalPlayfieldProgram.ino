e92055ec8e6ff2f88561a937a9baff9ad218349c
daugustine10 <103443273+daugustine10@users.noreply.github.com>
2024-11-22 11:40:27 -0700

    Add files via upload

--- FinalPlayfieldProgram.ino
+++ FinalPlayfieldProgram.ino
@@ -8,10 +8,6 @@
     the gutter has been triggered 3 times. 
  */
 
-#include <FastLED.h> // LED strip library declaration 
-#define DATA_PIN 6
-#define NUM_LEDS 10 
-CRGB leds[NUM_LEDS]; // can change the numbers later if we have to
 
 // Servo Motor
 #include <Servo.h> // Import the Servo library
